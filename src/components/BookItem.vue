9b7a76f2a402d5890cea8f721c83a2dd0f9dc8a6
Randy Boon <rwesterly2518@gmail.com>
2019-01-26 15:58:09 -0500

    Module 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>{{book.title}}:{{book.author}}</li> 
+</template>
+
+<script>
+export default {
+    name: 'BookItem',
+    props: ['book']
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li{
+    display: block;
+    margin: 0 10px;
+    font-family: Roboto;
+}
+
+</style>
